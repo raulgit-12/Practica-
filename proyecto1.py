2b46c9420c8923158e321f3407c3fa815f391d42
raulgit-12 <u21300629@utp.edu.pe>
2022-05-17 18:34:46 -0500

    789

---
+++ proyecto1.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+ventana = Tk()
+ventana.titile("Calculadora")
+
+# Entrada
+e_texto = Entry(ventana, front= ("calibri 20"))
+e_texto.grid(row = 0, colum = 0, columpspan = 4, padx = 5, pady =5)
